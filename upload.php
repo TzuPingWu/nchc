f441140a8e1cf1d423ed55af1e6be6bf440353d4
TzuPingWu <bottle7839@hotmail.com>
2013-10-04 14:50:53 +0800

    upload.php > 1. the multi-file can be uploaded at the same time! 	     2. the type-restriction for image-only is added.

--- upload.php
+++ upload.php
@@ -1,29 +1,30 @@
 <?php
-
-header("Content-Type: text/html; charset=utf-8");
 session_start();
+header("Content-Type: text/html; charset=utf-8");
 $uploaddir = md5($_SESSION['id'])."/";
-$uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
+//places files into md5(id) directory
+$filetype = array("image/jpeg","image/gif","image/bmp","image/png");
+print '<pre>';
+foreach ($_FILES["userfile"]["error"] as $key => $error) {
+	foreach($filetype as $typeKey=>$type){
+		if(!strcmp($filetype[$typeKey],$_FILES["userfile"]["type"][$key])){//check if the file is image
+			if(is_dir($uploaddir)){//check if the directory is existed.
+				move_uploaded_file( $_FILES["userfile"]["tmp_name"][$key],$uploaddir.$_FILES["userfile"]["name"][$key]) 
+			or die("Problems with upload");
+			}else{
+				mkdir(md5($_SESSION['id']));//build the new directory using md5(id) as name
+				move_uploaded_file( $_FILES["userfile"]["tmp_name"][$key],$uploaddir.$_FILES["userfile"]["name"][$key]) 
+				or die("Problems with upload");
+			}
+			break;
+		}//end of if-directory 
+		else{
+			echo "Problem with file-type!";
+		}
+	}//end of type-check loop foreach
+}//end of total-file loop foreach
 
-echo '<pre>';
-if(is_dir($uploaddir)){
-	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-		echo "File is valid, and was successfully uploaded.\\\\n";
-	} else {
-		echo "Possible file upload attack!\\\\n";
-	}
-}
-else{
-	mkdir(md5($_SESSION['id']),0777,true);
-	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-		echo "File is valid, and was successfully uploaded.\\\\n";
-	} else {
-		echo "Possible file upload attack!\\\\n";
-	}
-}
 echo 'Here is some more debugging info:';
 print_r($_FILES);
-
-print "</pre>";
-
+print '</pre>';
 ?>

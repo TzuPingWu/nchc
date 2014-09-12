9467cd443111adefb53660ec0c399378b7dee1bc
TzuPingWu <bottle7839@hotmail.com>
2014-09-13 01:11:42 +0800

    Update connect.php

--- SecondWork_v2/connect.php
+++ SecondWork_v2/connect.php
@@ -1,6 +1,6 @@
 <?php
 	header("Content-Type: text/html; charset=utf-8");
-	$db_link=@mysql_connect("dbhome.cs.nctu.edu.tw","tzpwu_cs","judy7839");
+	$db_link=@mysql_connect("dbhome.cs.nctu.edu.tw","","");
 	if(!$db_link) die("連線失敗");
 	if(!@mysql_select_db("tzpwu_cs")) die("資料庫選擇失敗"); 
 	mysql_query("SET NAMES 'utf8'"); 
